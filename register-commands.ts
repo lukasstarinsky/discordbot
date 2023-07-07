d01b1d93f3219682457c0813e74094220d822e24
Lukáš Starinský <lukas.starinsky8@gmail.com>
2023-07-07 19:20:01 +0200

    Fixed undefined env variables

--- register-commands.ts
+++ register-commands.ts
@@ -50,12 +50,12 @@ const commands = [
     }
 ];
 
-const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN!);
 
 (async () => {
   try {
     console.log("Registering commands...");
-    await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands });
+    await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID!, process.env.GUILD_ID!), { body: commands });
   } catch (err) {
     console.log(err);
   }
