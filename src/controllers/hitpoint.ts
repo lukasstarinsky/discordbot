751618ca03ea288346f79d18907ea8be905f651f
peto123451 <peto123455@gmail.com>
2024-07-06 10:56:53 +0200

    Update hitpoint command

--- src/controllers/hitpoint.ts
+++ src/controllers/hitpoint.ts
@@ -1,6 +1,7 @@
-import { CommandInteraction, ChatInputCommandInteraction, Message, EmbedBuilder } from "discord.js";
+import { CommandInteraction, ChatInputCommandInteraction, Message, EmbedBuilder, AttachmentBuilder } from "discord.js";
 import { load } from "cheerio";
 import axios from "axios";
+import Canvas from "@napi-rs/canvas";
 
 export async function HitPointStats(interaction: CommandInteraction) {
     const url = "https://lol.fandom.com/wiki/Hitpoint_3rd_Division/2024_Season/Summer_Season";
@@ -17,17 +18,53 @@ export async function HitPointStats(interaction: CommandInteraction) {
         if (isNaN(place) || place == 0) return;
 
         const name = $(tds[1]).text();
+        const logo = $(tds[1]).find("img").attr("src");
         const score = $(tds[2]).text();
-        stats.push({ place, name, score });
+
+        stats.push({ place, name, logo, score });
     });
 
-    const embed = new EmbedBuilder()
-        .setTitle("Hitpoint 3rd Division 2024 Summer Season")
-        .setColor(0x00fdfd);
+    const width = 800;
+    const rowHeight = 100;
+    const paddingTop = 150;
+    const height = stats.length * rowHeight + paddingTop;
+    const imageSize = 50;
+    const imageWidth = 120;
 
-    stats.forEach((s) => { 
-        embed.addFields({ name: s.place + ". " + s.name, value: "**" + s.score + "**" });
-    });
+    const canvas = Canvas.createCanvas(width, height);
+    const context = canvas.getContext("2d");
 
-    interaction.editReply({ embeds: [embed] });
+    context.fillStyle = "#505050";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.textAlign = "center";
+    context.textBaseline = "middle";
+    context.font = "50px Calibri";
+    context.fillStyle = "#FFFFFF";
+    context.fillText("Hitpoint 3rd Division Summer Season", width / 2, 50);
+
+    for (let i = 0; i < stats.length; i++) {
+        context.textAlign = "left";
+        context.textBaseline = "middle";
+        context.font = "40px Calibri";
+        context.fillStyle = "#FFFFFF";
+        const img = await Canvas.loadImage(stats[i].logo);
+        context.drawImage(img, 50, i * rowHeight + paddingTop - imageSize / 2, imageWidth, imageSize);
+
+        if (i == 0 || stats[i].place != stats[i - 1].place) {
+            context.fillText(stats[i].place + ".", 25, i * rowHeight + paddingTop);
+        } else {
+            context.fillText("-", 25, i * rowHeight + paddingTop);
+        }
+    
+
+        context.fillText(stats[i].name, 175, i * rowHeight + paddingTop);
+        
+        context.textAlign = "right";
+        context.fillText(stats[i].score, width - 25, i * rowHeight + paddingTop);
+
+        console.log("Drawing image " + stats[i].name);
+    }
+    
+    const attachment = new AttachmentBuilder(await canvas.encode("png"), { name: "ingame_data.png" });
+    await interaction.editReply({ files: [attachment] });
 }
