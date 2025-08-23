bb9acc06effc1de43d0ec9fe61e8799f4dc7ad8d
lukasstarinsky <lukas.starinsky8@gmail.com>
2025-08-23 20:18:02 +0200

    Fix global commands

--- src/register-commands.ts
+++ src/register-commands.ts
@@ -225,7 +225,7 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN!);
 (async () => {
     try {
         console.log("Registering commands...");
-        await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID!, process.env.GUILD_ID!), { body: commands });
+        await rest.put(Routes.applicationCommands(process.env.CLIENT_ID!), { body: commands });
     } catch (err) {
         console.log(err);
     }
